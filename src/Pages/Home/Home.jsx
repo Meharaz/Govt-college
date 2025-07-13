a1869aa1c1dcd56681143bf9e236eab9a7e4d92f
noman <ariyan.sacc@gmail.com>
2025-07-14 03:54:31 +0600

    main section addedd

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AutoSlider from './AutoSlider/AutoSlider';
+import MainSection from './MainSection/MainSection';
 
 const Home = () => {
     return (
         <div>
             <AutoSlider />
+            <MainSection />
         </div>
     );
 };
