6932d975d4037926b0c0a5554f34037d32b90751
noman <ariyan.sacc@gmail.com>
2025-07-14 00:49:16 +0600

    navbar added

--- src/Shared/NavBar/Navbar.jsx
+++ src/Shared/NavBar/Navbar.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 
 const Navbar = () => {
+    const navOptions = <>
+        <li><a className="hover:bg-yellow-500 transition-colors duration-300">Home</a></li>
+        <li><a className="hover:bg-yellow-500 transition-colors duration-300">About</a></li>
+        <li><a className="hover:bg-yellow-500 transition-colors duration-300">Academic</a></li>
+        <li><a className="hover:bg-yellow-500 transition-colors duration-300">Admission</a></li>
+        <li><a className="hover:bg-yellow-500 transition-colors duration-300">Result</a></li>
+    </>;
+
     return (
-        <div className="navbar bg-base-100 flex">
-            <div className=" ml-16 flex-1">
-                <img className="btn sm:btn-sm md:btn-md lg:btn-lg btn-ghost normal-case" src="src/assets/Logo/Main Logo.png" alt="Bandarban Govt Mohila College" />
-            </div>
-            <div className='flex-1 text-2xl'>
-                বান্দরবান সরকারি মহিলা কলেজ 
-            </div>
-            <div className="flex-1">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Link</a></li>
-                    <li>
-                        <details>
-                            <summary>
-                                Parent
-                            </summary>
-                            <ul className="p-2 bg-base-100">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
+        <div className='fixed top-0 w-full z-50'>
+            <div className="navbar bg-[#00809D] text-white font-serif justify-between">
+                
+                {/* Mobile Dropdown (Left) */}
+                <div className="navbar-start lg:hidden">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
+                                 viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                      d="M4 6h16M4 12h8m-8 6h16"/>
+                            </svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52">
+                            {navOptions}
+                        </ul>
+                    </div>
+                </div>
+
+                {/* Desktop Menu (Center) */}
+                <div className="hidden lg:flex lg:mx-auto">
+                    <ul className="menu menu-horizontal px-1 font-bold">
+                        {navOptions}
+                    </ul>
+                </div>
+
+                {/* Right Spacer (empty to help justify-between layout) */}
+                <div className="navbar-end lg:hidden"></div>
             </div>
         </div>
     );
